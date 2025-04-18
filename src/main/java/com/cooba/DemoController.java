c0a5b1a4635d5cd406e4cf5148ba2774c08b083a
aiden <aidenlin@star-link.tech>
2025-04-18 09:14:43 +0800

    first commit

---
+++ src/main/java/com/cooba/DemoController.java
@@ -0,0 +1,28 @@
+package com.cooba;
+
+
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    private final Tracer tracer;
+
+    public DemoController(Tracer tracer) {
+        this.tracer = tracer;
+    }
+
+    @GetMapping
+    public String traceDemo() {
+        Span span = tracer.currentSpan();
+        if (span != null) {
+            span.tag("custom.tag", "demo-call");
+        }
+        return "Tracing to Jaeger!";
+    }
+}
